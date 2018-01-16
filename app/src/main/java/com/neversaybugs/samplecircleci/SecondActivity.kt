2fa54f198f9d6c6e81262189102599375a363d1c
Jeeraphan <jeeraphan.lairat@gmail.com>
2018-01-16 23:09:53 +0700

    add kotlin

---
+++ app/src/main/java/com/neversaybugs/samplecircleci/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.neversaybugs.samplecircleci
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by jeeraphan on 1/16/18.
+ */
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
