f1d1ae988bd3ed6ca75d52c9cc8736180120e5ee
Jeeraphan <jeeraphan.lairat@gmail.com>
2018-01-21 21:27:47 +0700

    refactor code

--- app/src/main/java/samplecircleci/MainActivity.kt
+++ app/src/main/java/samplecircleci/MainActivity.kt
@@ -1,15 +1,11 @@
-package com.neversaybugs.samplecircleci
+package samplecircleci
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-/**
- * Created by jeeraphan on 1/16/18.
- */
-
-class SecondActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
